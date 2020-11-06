75e1f041ea6bc1355422f8d22c4b7fa46bbc451b
raleighy <raleighy@163.com>
2020-11-06 11:18:25 +0800

    v0.1.6

--- scripts/init.js
+++ scripts/init.js
@@ -1,6 +1,7 @@
-const fs = require("fs-extra")
+const fs = require("fs")
 const path = require("path")
 const copyDirSync = require("../lib/copy").copyDirSync
+const updateIgnored = require("../lib/updateIgnored")
 
 module.exports = function init(projectName) {
   const appDirectory = fs.realpathSync(process.cwd())
@@ -17,17 +18,5 @@ module.exports = function init(projectName) {
     //   }
     // }
   )
-
-  const gitignoreExists = fs.existsSync(resolveApp(".gitignore"))
-  if (gitignoreExists) {
-    // Append if there's already a `.gitignore` file there
-    const data = fs.readFileSync(resolveApp("gitignore"))
-    fs.appendFileSync(resolveApp(".gitignore"), data)
-    fs.unlinkSync(resolveApp("gitignore"))
-  } else {
-    // Rename gitignore after the fact to prevent npm from renaming it to .npmignore
-    // See: https://github.com/npm/npm/issues/1862
-    fs.moveSync(resolveApp("gitignore"), resolveApp(".gitignore"), [])
-  }
-  fs.moveSync(resolveApp("yarnlock"), resolveApp("yarn.lock"), [])
+  updateIgnored()
 }
---
+++ scripts/publish.js
@@ -0,0 +1,39 @@
+const fs = require("fs-extra")
+const path = require("path")
+
+const template = (str) => path.resolve("package/templates", str)
+const main = (str) => path.resolve("package/main", str)
+
+const updateArr = [
+  {
+    from: template("react-runtime/yarn.lock"),
+    to: template("react-runtime/yarnlock"),
+  },
+  {
+    from: template("react-runtime/.gitignore"),
+    to: template("react-runtime/gitignore"),
+  },
+  {
+    from: template("vue-runtime/yarn.lock"),
+    to: template("vue-runtime/yarnlock"),
+  },
+  {
+    from: template("vue-runtime/.gitignore"),
+    to: template("vue-runtime/gitignore"),
+  },
+  {
+    from: main(".gitignore"),
+    to: main("gitignore"),
+  },
+  {
+    from: main("yarn.lock"),
+    to: main("yarnlock"),
+  },
+]
+
+updateArr.forEach((item) => {
+  const fileExists = fs.existsSync(item.from)
+  if (fileExists) {
+    ;(item.replace ? fs.moveSync : fs.copyFileSync)(item.from, item.to)
+  }
+})
